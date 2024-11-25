1edf8520b4125e041cedeb12c885608a4cf57406
Dinar Khazipov <khazipovdn@yandex.ru>
2024-11-26 00:46:08 +0300

    ТЗ 12 ver12

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.user.UserStorage;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.myException.ResourceNotFoundException;
 
 import java.util.*;
 
